0646aeefc6f4fcbb96408e0eae622233e85f02ba
Daniel Boaitey <boaiteyd7@gmail.com>
2018-11-26 18:49:47 -0500

    Sales page populated

--- client/sales.js
+++ client/sales.js
@@ -1,3 +1,32 @@
 window.onload = () => {
     loadNavbar();
+	get("salesAgent")
+        .then(data => {
+            for (let sale of data) {
+                createSale(sale)
+            }
+        })
+        .catch(e => {
+            console.error(e);
+            document.querySelector(".error").classList.remove("hidden");
+        });
 };
+
+function createSale(sale) {
+    const accountType = getAccountType();
+    const saleHTML = `
+        <div class="sale">
+            <div class="sale-info">
+                <h3 class="s_agent">Price: $${sale.sell_price} Time Sold: ${sale.time_sold}</h3>
+				<h3 class="title">Client Info: ${sale.first_name} ${sale.last_name} - ${sale.email}</h3>
+			
+            </div>
+            
+    
+        </div>
+    `;
+    
+    const saleContainer = document.getElementById("sales-container");
+    
+    saleContainer.innerHTML += saleHTML;
+}
